7762bae45ad61c6e45d695120552919166547b8e
Chival Trotman <chivy360@gmail.com>
2017-03-20 14:15:05 -0400

    first commit

--- src/server.js
+++ src/server.js
@@ -2,7 +2,21 @@ const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const path = require("path");
+const port = process.env.PORT || 8080;
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser")
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+require("dotenv").config({path: "./src/email.env"});
 
+app.use(urlencodedParser);
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.GMUSER,
+        pass: process.env.GMPASS
+    }
+});
 app.set('views', path.resolve(__dirname + "/static/views"));
 app.set("view engine", "ejs");
 
@@ -20,20 +34,37 @@ app.get("/", (req, res) => {
     res.render("about.ejs");
 });
 
-app.get("/contact", (err, req, res) => {
-    if (err) {
-        return next(err);
-    }
-    res.render("contact");
+app.get("/about", (req, res) => {
+    res.render("about.ejs")
+})
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
 });
 
-app.get("/projects", (err, req, res) => {
-    if (err) {
-        return next(err);
-    }
-    res.render("projects");
+app.get("/projects", (req, res) => {
+    res.render("projects.ejs");
 });
+app.post("/send", (req, res) => {
+    let mailOptions = {
+    from: "<" + req.body.address + ">",
+    to: "chivy360@gmail.com", 
+    subject: req.body.subject,
+    text: req.body.message, 
+    html: '<p>' + req.body.message + '</p>'
+};
+    transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+    res.redirect("/about");
+    });
+});
+app.get("*", (req, res) => {
+    res.render("404.ejs");
+})
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Listening on port 3000");
 });
